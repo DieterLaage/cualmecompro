2613ba19136c6b6d0d19e46861a04ac172c17322
DieterLaage <DIETER.LAAGE.M@MAIL.PUCV.CL>
2026-06-04 11:30:59 -0400

    Migracion inicial a React Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+// function App() {
+//   return (
+//     <div style={{ padding: "2rem" }}>
+//       <h1>QueCompro</h1>
+
+//       <input
+//         type="text"
+//         placeholder="Buscar producto..."
+//         style={{
+//           width: "300px",
+//           padding: "10px",
+//           marginRight: "10px"
+//         }}
+//       />
+
+//       <button>Buscar</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">
+        Tailwind funcionando 🚀
+      </h1>
+    </div>
+  )
+}
